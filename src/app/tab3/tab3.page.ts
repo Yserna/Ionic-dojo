afa89a15a62fbdba7db2ba359eed02e9c3e1fc0e
Jaime Jimenez Serrano <jaime.jimenezs@udea.edu.co>
2019-07-13 09:29:24 -0500

    Create dojo Ionic

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera } from '@ionic-native/camera/ngx'
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,36 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  foto: any;
+
+  constructor(private camera: Camera) {}
+
+  capturarFoto() {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    }).then(dataImage => {
+      this.foto = 'data:image/jpeg;base64,'+dataImage;
+    })
+  }
+
+  buscarFoto() {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }).then(dataImage => {
+      this.foto = 'data:image/jpeg;base64,'+dataImage;
+    })
+  }
 
 }
